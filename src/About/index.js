525dc3c71d5f5570bc1199101afd4f0fd88e0409
Latifa-Umunyana <latifahumunyana@gmail.com>
2024-06-21 16:38:22 +0300

    adding files

---
+++ src/About/index.js
@@ -0,0 +1,21 @@
+
+
+import './index.css';
+
+const About = () =>{
+
+return(
+
+<div className="about-text">
+
+<p>We are learning about JSX</p>
+<p>We rae coding in a new Front-end library</p>
+
+</div>
+
+)
+
+
+}
+
+export default About;
